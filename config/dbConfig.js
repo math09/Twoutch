ccf1093cb2faf694ac9c9f5b084697d381c255ef
DRmatteo74 <mdirienzo74@gmail.com>
2024-07-23 09:17:58 +0200

    create mongoose connexion with mongodb

---
+++ config/dbConfig.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://root:UyE4emyXawSzZ3o7@twoutch.pkllym2.mongodb.net/?retryWrites=true&w=majority&appName=Twoutch")
+
+export default mongoose;
