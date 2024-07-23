e46a326405854dbe78f8cf78eefeac8aac069cfa
math09 <hernandezmathis@gmail.com>
2024-07-23 14:04:19 +0200

    fix error

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,11 +1,10 @@
 import e from "express";
 import userController from "../controllers/userController.js";
-import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = e.Router();
 
 router.get("/", userController.getAllUsers);
-router.get("/:id",authMiddleware.authenticateToken, userController.getUser);
+router.get("/:id", userController.getUser);
 router.post("/", userController.createUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
