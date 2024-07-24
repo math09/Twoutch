383c416d153e71832e5020f0b3edf259e76c037a
DRmatteo74 <mdirienzo74@gmail.com>
2024-07-24 11:08:22 +0200

    Add all routes in swagger doc

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import http from 'http';
 import routes from './routes/routes.js';
 import config from './config/config.js';
 import mongoose from 'mongoose';
@@ -9,7 +8,6 @@ import logger from './utils/logger.js';
 import swaggerDocument from './swagger.json' assert { type: 'json' };
 
 const app = express();
-const server = http.createServer(app);
 app.use(express.json());
 
 // Security
@@ -21,7 +19,6 @@ mongoose
   .connect(config.dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => logger.info("Connected to MongoDB"))
   .catch((err) => logger.error("Could not connect to MongoDB", err));
-
 
 
 // Swagger
